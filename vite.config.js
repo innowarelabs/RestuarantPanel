0ea7ba837c745f2e46378e9268770ccafdf1d16b
irfanulhaq228 <120976490+irfanulhaq228@users.noreply.github.com>
2026-03-06 01:19:12 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5175,
     host: '0.0.0.0',
+  },
+  preview: {
+    port: 6901,
+    host: '0.0.0.0',
+    allowedHosts: ['restaurant.baaie.com'],
   },
 })
